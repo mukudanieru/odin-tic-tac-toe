d71f7e8fac23acfaa8af0e9793e4640dcd7f6974
mukudanieru <111953203+mukudanieru@users.noreply.github.com>
2025-10-26 20:30:42 +0800

    Add board object

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Player
-function createPlayer(name, marker) {
+function Player(name, marker) {
     let score = 0;
 
     return {
@@ -18,5 +18,55 @@ function createPlayer(name, marker) {
     };
 }
 
-const mukudanieru = createPlayer("mukudanieru", "X");
-const lone = createPlayer("lone", "O");
+// Cell - basically each of the cell in a tic tac toe
+function Cell() {
+    let value = 0;
+
+    return {
+        addValue(playersMarker) {
+            value = playersMarker;
+        },
+        getValue() {
+            return value;
+        },
+    };
+}
+
+// Board
+function Gameboard() {
+    const ROW = 3,
+        COLUMN = 3;
+
+    const board = [];
+
+    for (let i = 0; i < ROW; i++) {
+        board[i] = [];
+
+        for (let j = 0; j < COLUMN; j++) {
+            board[i].push(Cell());
+        }
+    }
+
+    return {
+        getBoard() {
+            return board;
+        },
+        printBoard() {
+            for (let i = 0; i < 3; i++) {
+                let row = "";
+                for (let j = 0; j < 3; j++) {
+                    row += board[i][j].getValue();
+                }
+                console.log(row);
+            }
+        },
+    };
+}
+
+const board = Gameboard().printBoard();
+
+// Game Controller
+function gameController() {}
+
+// Dom Controller
+function DOMController() {}
